c1243e29b83cdfd40923d19ee4e72f69f4bdd1f3
Léo Peyré <leo.peyre95@gmail.com>
2020-02-27 16:06:52 +0100

    wch

--- Billionar'Y/public/js/navbar.js
+++ Billionar'Y/public/js/navbar.js
@@ -1,19 +1,10 @@
 jQuery(function($) {
-	$('.header-nav__wrapper').on('click', function() {
-		// $('.nav--responsive').toggle();
-		$('.nav--responsive').addClass('is-open');
-		$('overlay').toggle();
-		$('.header-nav-burger').addClass('is-animate');
-		setTimeout(function() {
-			$('.overlay').addClass('is-open');
-		}, 500);
-		$('.overlay').toggle();
-		$('body').addClass('overflow');
-	});
-	$('.overlay').on('click', function() {
-		// $('.nav--responsive').toggle();
-		$('.nav--responsive').removeClass('is-open');
-		$('.overlay').toggle();
-		$('.header-nav-burger').removeClass('is-animate');
-		$('body').removeClass('overflow');
-	});
+
+    console.log("CCCCC");
+	$('.nav-item-container__wrapper').click(function(){
+        $('.nav-item-container__wrapper').toggleClass('.flex');
+        });
+
+
+
+      });
