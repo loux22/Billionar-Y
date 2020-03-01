4c5abe94488f4578f5f897afbd86100dab5eefff
loux22 <louis.ardilly@yahoo.fr>
2020-03-01 20:37:12 +0100

    add js navbar

--- Billionar'Y/public/js/navbar.js
+++ Billionar'Y/public/js/navbar.js
@@ -1,10 +1,6 @@
 jQuery(function($) {
 
-    console.log("CCCCC");
-	$('.nav-item-container__wrapper').click(function(){
-        $('.nav-item-container__wrapper').toggleClass('.flex');
+	$('.nav-item-container').click(function(){
+        $('.nav-item-container__wrapper').toggle();
         });
-
-
-
       });
